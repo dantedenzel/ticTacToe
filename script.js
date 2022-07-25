78b9262bcbf7a84ab6142b41d30aca9245ede9e6
danteburnett <danteburnett@gmail.com>
2022-07-25 14:57:53 -0700

    first finish

--- script.js
+++ script.js
@@ -20,14 +20,54 @@ const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `Its's ${currentPlayer}'s turn`;
 // We set the initial message to let the players know whose turn it is
 statusDisplay.innerHTML = currentPlayerTurn();
-function handleCellPlayed() {
-
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+// We update our internal game state to reflect the played move, as well as update the user interface to reflect the played move
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
 }
 function handlePlayerChange() {
-
+    currentPlayer = currentPlayer == "X" ? "0" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
 }
-function resultValidation() {
-
+const winngConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b ==='' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+if (roundWon) {
+    statusDisplay.innerHTML = winningMessage();
+    gameActive = false;
+    return;
+    }
+    // Check whether there are any values in our game state array that are still not populated with a player sign
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+    // if we get to here, we know that no ones won the game yet and that there are still moves to be played, so we continue by changing the current player.
+    handlePlayerChange();
 }
 function handleCellClick(clickedCellEvent) {
 //  Save the clicked html element in a variable for later use
@@ -50,7 +90,12 @@ Next up we need to check whether the call has already been played, or IF the gam
 }
 
 function handleRestartGame() {
-
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""]
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell')
+                .forEach(cell => cell.innerTML = "");
 }
 // Finally we add our event listeners to the actual game cells, as well as our restart button
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
