f83eaba0b39280ea5a4502be32f24f121a366ed8
danteburnett <danteburnett@gmail.com>
2022-07-25 14:00:58 -0700

    js comments

---
+++ script.js
@@ -0,0 +1,23 @@
+// store our game status element here to allow us to more easily use it later
+
+
+// Declare variables that we can use to track the state of the game as its played
+
+
+// We will us gameActive to pause the game in case of an end scenario
+
+
+// We will store our current player here, so we know who's turn it is
+
+/*  We will store our current game state here, in the form of empty strings in an array
+    will allow us to easily track played cells and validate the game state later on 
+*/
+
+/* Declare messages to display to the user during the game.
+    Since we have some dynamic factors in the messages, namely the current player,
+    we have to declare them as FUNCTIONS, so that the actual message gets created with 
+    current data every time we need it. 
+*/
+
+// We set the initial message to let the players know whose turn it is
+
